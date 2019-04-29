9219a4a44cee86b82a4212e739edc48a78258cea
othalla <othalla.lf@gmail.com>
2019-04-29 18:45:27 +0200

    parametrize module with url, revision & directory

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,54 +1,14 @@
-# Class: dns_zones
-# ===========================
-#
-# Full description of class dns_zones here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'dns_zones':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2018 Your name here, unless otherwise noted.
-#
-class dns_zones {
+class dns_zones (
+  String $dns_zones_url,
+  String $dns_zones_directory,
+  String $revision = 'master',
+) {
 
-  vcsrepo { '/var/named/':
+  vcsrepo { $dns_zones_directory:
     ensure   => present,
     provider => git,
     remote   => 'origin',
-    revision => 'master',
-    source   => 'https://github.com/othalla/dns.git',
+    revision => $revision,
+    source   => $dns_zones_url,
   }
 }
